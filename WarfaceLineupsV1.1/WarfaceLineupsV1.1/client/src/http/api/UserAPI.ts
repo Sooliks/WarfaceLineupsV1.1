c9dcee9ae1cc43204f83cdb66c54b0442406603f
Sooliks <evgenij.terexov.2000@mail.ru>
2023-09-15 16:05:55 +0300

    gjgjjghfhhg

--- WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/http/api/UserAPI.ts
+++ WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/http/api/UserAPI.ts
@@ -8,4 +8,13 @@ export namespace UserAPI {
     export const authorization = async (email: string, password: string): Promise<AxiosResponse> => {
         return await $client.post('/authorization', {email, password});
     }
+    export const authorizationByJwt = async (login: string, jwtToken: string): Promise<AxiosResponse> => {
+        return await $client.post('/authorizationbyjwt', {login, jwtToken});
+    }
+    export const getVerificationCode = async (): Promise<AxiosResponse> => {
+        return await $clientAuth.get('/getverificationcode');
+    }
+    export const uploadVerificationCode = async (verificationCode: string): Promise<AxiosResponse> => {
+        return await $clientAuth.post('/uploadverificationcode', verificationCode);
+    }
 }
