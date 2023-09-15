24a308cfaf278aa3e3d5c5fa884424592fca351a
Sooliks <evgenij.terexov.2000@mail.ru>
2023-09-15 20:31:06 +0300

    hffh

--- WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/App.tsx
+++ WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AppRouter from "./AppRouter";
 import {BrowserRouter} from "react-router-dom";
 import classes from './Main.module.css'
 
-import {ConfigProvider, theme} from "antd";
+import {ConfigProvider, Spin, theme} from "antd";
 import NavigationContextProvider from "./context/NavigationContextProvider";
+import {UserAPI} from "./http/api/UserAPI";
+import {cookies} from "./data/cookies";
+import {defaultUser, useUserContext} from "./context/UserContextProvider";
+
 
 function App() {
-  return (
+    const[loading, setLoading] = useState<boolean>(true);
+    const userContext = useUserContext();
+    useEffect(()=>{
+        UserAPI.authorizationByJwt(cookies.get('login'),cookies.get('jwt')).then(res=> {
+            if(res.status === 200){
+                userContext.setUser({...userContext.user, account: res.data})
+                userContext.setUser({...userContext.user, isAuth: true})
+                setLoading(false);
+            }
+            else {
+                setLoading(false);
+                userContext.setUser(defaultUser)
+            }
+        }).catch((e)=>{
+
+        })
+    },[])
+    if(loading){
+        return <Spin size="large" className={classes.spinnerLoading} />
+    }
+
+    return (
       <ConfigProvider theme={{
           algorithm: theme.darkAlgorithm,
       }}>
           <NavigationContextProvider>
-              <div className={classes.main}>
-                <BrowserRouter>
-                    <AppRouter/>
-                </BrowserRouter>
-              </div>
+            <BrowserRouter>
+                <AppRouter/>
+            </BrowserRouter>
           </NavigationContextProvider>
       </ConfigProvider>
-  );
+    );
 }
 
 export default App;
--- WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/index.tsx
+++ WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import UserContextProvider from "./context/UserContextProvider";
 
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <UserContextProvider>
+          <App />
+      </UserContextProvider>
   </React.StrictMode>
 );
 
