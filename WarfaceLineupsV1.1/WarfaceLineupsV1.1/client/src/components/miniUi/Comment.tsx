a95d214ca04d87d96b75770a67227517e0226919
Sooliks <evgenij.terexov.2000@mail.ru>
2023-09-20 14:23:38 +0300

    start ready

---
+++ WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/components/miniUi/Comment.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comment: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Comment;
--- WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/pages/Start.tsx
+++ WarfaceLineupsV1.1/WarfaceLineupsV1.1/client/src/pages/Start.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
+import {Button, Carousel, Space, Typography} from "antd";
+import {useNavigate} from "react-router-dom";
+import {useNavigationContext} from "../context/NavigationContextProvider";
 
+const { Text, Title } = Typography;
 const Start: React.FC = () => {
-    return (
-        <div>
+    const navigate = useNavigate();
+    const navigationContext = useNavigationContext();
 
+    const insideContentStyle: CSSProperties = {
+        height: 350,
+        lineHeight: '350px',
+        background: '#1e1d1d',
+        textAlign: 'center',
+    }
+    return (
+        <div style={{width: '100%', height: '100%'}}>
+            <div style={{width: '100%'}}>
+                <Carousel autoplay style={{width: '100%'}}>
+                    <Space>
+                        <div style={insideContentStyle}>
+                            <Text style={{fontSize: '14pt'}}>Warface Lineups - это сайт где вы можете загрузить свои раскидки и в будущем сетапы, а также искать их</Text>
+                        </div>
+                    </Space>
+                    <Space>
+                        <div style={insideContentStyle}>
+                            <Text style={{fontSize: '14pt'}}>Warface Tracker - это сервис для просмотра статистики игры</Text>
+                        </div>
+                    </Space>
+                </Carousel>
+            </div>
+            <Space direction={"vertical"} style={{display:'flex', alignItems: 'center'}}>
+                <Title>Warface Lineups</Title>
+                <Space direction={"horizontal"}>
+                    <Button type={"primary"} onClick={()=>{
+                        navigationContext.setNavigation({currentPage: "/profile"});
+                        navigate("/profile");
+                    }}>Начать</Button>
+                </Space>
+            </Space>
         </div>
     );
 };
